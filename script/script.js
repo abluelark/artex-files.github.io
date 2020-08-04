57d01555f35f27ef540df2bc3c3d1fa1b81d55c7
Artex <clarka94@gmail.com>
2020-08-04 08:29:02 -0700

    Update script.js

--- script/script.js
+++ script/script.js
@@ -30,7 +30,7 @@ const Config = {
             "greenway",
             [
                 ["minor behavior form", "https://docs.google.com/forms/d/1GFrl_qV0JUPh4A3lUid9RkRvvH2DIOGp2rP9P7s2L9g/viewform?ts=5f20f778&edit_requested=true"],
-                ["link", "https://www.example.com"],
+                ["staff directory", "https://docs.google.com/spreadsheets/d/1u6mBfl6CDg--tE_8vJ1dQ323XNTYv560U10PEaW6Dcw/edit?ts=5f285432#gid=0"],
                 ["link", "https://www.example.com"],
                 ["link", "https://www.example.com"]
             ]
